c95d1b1a4dc38c64da0f9eb7eb28cd3a7b3d5f90
maikals <miquicm@gmail.com>
2014-11-28 15:24:02 +0100

    First commit 2.0

--- app/src/main/java/com/pes/maikals/quiosquer/MainActivity.java
+++ app/src/main/java/com/pes/maikals/quiosquer/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+
 public class MainActivity extends Activity {
 
     @Override
@@ -28,7 +29,6 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
